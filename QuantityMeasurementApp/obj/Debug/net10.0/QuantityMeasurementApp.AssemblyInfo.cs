d9e3f43f50d492c62bdea83ed4aada225899f220
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-02-23 12:44:39 +0530

    [Priyanshu chauhan] Add : Implemented Extended Unit Support

--- QuantityMeasurementApp/obj/Debug/net10.0/QuantityMeasurementApp.AssemblyInfo.cs
+++ QuantityMeasurementApp/obj/Debug/net10.0/QuantityMeasurementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ef01f2d505ed3db0115b5d10fdefe371f4c9d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb8782a4019be8d6bb70d204004eb17e946146e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
