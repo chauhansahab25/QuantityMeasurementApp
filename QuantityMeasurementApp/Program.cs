62ef01f2d505ed3db0115b5d10fdefe371f4c9d2
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-02-23 11:39:17 +0530

    [Priyanshu Chauhan] Add : Implemented Feet Measurement Equality

---
+++ QuantityMeasurementApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using QuantityMeasurementApp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Feet Measurement Equality Check");
+        Console.WriteLine("--------------------------------");
+
+        Console.Write("Enter first feet value: ");
+        double value1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second feet value: ");
+        double value2 = Convert.ToDouble(Console.ReadLine());
+
+        Feet f1 = new Feet(value1);
+        Feet f2 = new Feet(value2);
+
+        bool result = f1.Equals(f2);
+
+        Console.WriteLine();
+        Console.WriteLine("Input: " + value1 + " ft and " + value2 + " ft");
+        Console.WriteLine("Output: Equal (" + result + ")");
+
+        Console.ReadLine();
+    }
+}
