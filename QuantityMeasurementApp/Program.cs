5c207fdf7a9cf8263677298ce6d5ff5211c3b9d3
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-02-23 12:29:55 +0530

    [Priyanshu Chauhan] Add : Implemented Genric length

--- QuantityMeasurementApp/Program.cs
+++ QuantityMeasurementApp/Program.cs
@@ -1,53 +1,37 @@
 ﻿using System;
-using QuantityMeasurementApp;
+using QuantityMeasurementApp.Model;
 
-public class Program
+namespace QuantityMeasurementApp
 {
-    public static bool CompareFeet(double value1, double value2)
+    class Program
     {
-        Feet f1 = new Feet(value1);
-        Feet f2 = new Feet(value2);
-        return f1.Equals(f2);
-    }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter value:");
+                double value = Convert.ToDouble(Console.ReadLine());
 
-    public static bool CompareInches(double value1, double value2)
-    {
-        Inches i1 = new Inches(value1);
-        Inches i2 = new Inches(value2);
-        return i1.Equals(i2);
-    }
+                Console.WriteLine("Enter unit (FEET / INCH):");
+                string unitInput = Console.ReadLine()?.ToUpper();
 
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Quantity Measurement Equality Check");
-        Console.WriteLine("----------------------------------");
+                Unit unit = unitInput switch
+                {
+                    "FEET" => Unit.FEET,
+                    "INCH" => Unit.INCH,
+                    _ => throw new ArgumentException("Invalid unit entered")
+                };
 
-        Console.Write("Enter first feet value: ");
-        double feet1 = Convert.ToDouble(Console.ReadLine());
+                Quantity quantity = new Quantity(value, unit);
 
-        Console.Write("Enter second feet value: ");
-        double feet2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Quantity created: {quantity.Value} {quantity.Unit}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
 
-        bool feetResult = CompareFeet(feet1, feet2);
-
-        Console.WriteLine();
-        Console.WriteLine("Input: " + feet1 + " ft and " + feet2 + " ft");
-        Console.WriteLine("Output: Equal (" + feetResult + ")");
-
-        Console.WriteLine();
-
-        Console.Write("Enter first inches value: ");
-        double inch1 = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Enter second inches value: ");
-        double inch2 = Convert.ToDouble(Console.ReadLine());
-
-        bool inchResult = CompareInches(inch1, inch2);
-
-        Console.WriteLine();
-        Console.WriteLine("Input: " + inch1 + " inch and " + inch2 + " inch");
-        Console.WriteLine("Output: Equal (" + inchResult + ")");
-
-        Console.ReadLine();
+            Console.ReadLine();
+        }
     }
 }
