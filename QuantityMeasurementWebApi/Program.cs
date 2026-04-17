ce3817e344a07bc09db4ef20bf72e1c7f29e64e8
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-04-17 16:30:52 +0530

    Fix DATABASE_URL parsing - handle missing port and query params

--- QuantityMeasurementWebApi/Program.cs
+++ QuantityMeasurementWebApi/Program.cs
@@ -43,7 +43,9 @@ if (!string.IsNullOrEmpty(databaseUrl))
     // Parse postgres://user:password@host:port/database mapping to Npgsql connection string
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432; // Default PostgreSQL port if not specified
+    var password = userInfo.Length > 1 ? userInfo[1] : "";
+    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/').Split('?')[0]};Username={userInfo[0]};Password={password};SSL Mode=Require;Trust Server Certificate=true";
 }
 else if (string.IsNullOrEmpty(connectionString))
 {
